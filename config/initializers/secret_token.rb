4b70f7e565c05e51289debab62cba059587a9b1f
Your Name <everton2x4@gmail.com>
2012-06-07 21:11:49 -0300

    new Rails app generated by Rails Apps Composer gem

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+UspLostAndFound::Application.config.secret_token = '5f9638e45183449e62e18e9240aea51e9d868e56b8d5b8536a50a5de6da380008626b8dd0c5f32c94502e47a6fed20a83550323abbbc0cac1f4c09d1074652d7'
