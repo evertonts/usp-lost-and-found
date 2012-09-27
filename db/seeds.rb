70673deb10975be0716c2f1a1faca5f55b126f1f
Your Name <everton2x4@gmail.com>
2012-09-27 00:43:59 -0300

    Arrumando os seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,38 +19,38 @@ item = Item.new title: "[test] Carteira", description: "Carteira preta com detal
 item.user = user
 item.save!
 
-item = Item.create! title: "[test] Oculos", description: "Oculos preto perdido no IME", lost_date: Time.now.to_date.strftime("%d/%m/%Y"), lost: true
+item = Item.new title: "[test] Oculos", description: "Oculos preto perdido no IME", lost_date: Time.now.to_date.strftime("%d/%m/%Y"), lost: true
 item.user = user2
 item.save!
 
-item = Item.create! title: "[test] Blusa", description: "Blusa azul da GAP perdida na FEA", lost_date: Time.now.to_date.strftime("%d/%m/%Y"), lost: true
+item = Item.new title: "[test] Blusa", description: "Blusa azul da GAP perdida na FEA", lost_date: Time.now.to_date.strftime("%d/%m/%Y"), lost: true
 item.user = user
 item.save!
 
-item = Item.create! title: "[test] Estojo", description: "Estojo azul com ziper preto", lost_date: Time.now.to_date.strftime("%d/%m/%Y"), lost: true
+item = Item.new title: "[test] Estojo", description: "Estojo azul com ziper preto", lost_date: Time.now.to_date.strftime("%d/%m/%Y"), lost: true
 item.user = user2
 item.save!
 
-item = Item.create! title: "[test] Mochila", description: "Mochila amarela", lost_date: Time.now.to_date.strftime("%d/%m/%Y"), lost: true
+item = Item.new title: "[test] Mochila", description: "Mochila amarela", lost_date: Time.now.to_date.strftime("%d/%m/%Y"), lost: true
 item.user = user
 item.save!
 
-item = Item.create! title: "[test] Carteira", description: "Carteira preta com detalhes marrons", lost_date: Time.now.to_date.strftime("%d/%m/%Y"), lost: false
+item = Item.new title: "[test] Carteira", description: "Carteira preta com detalhes marrons", lost_date: Time.now.to_date.strftime("%d/%m/%Y"), lost: false
 item.user = user2
 item.save!
 
-item = Item.create! title: "[test] Oculos", description: "Oculos preto perdido no IME", lost_date: Time.now.to_date.strftime("%d/%m/%Y"), lost: false
+item = Item.new title: "[test] Oculos", description: "Oculos preto perdido no IME", lost_date: Time.now.to_date.strftime("%d/%m/%Y"), lost: false
 item.user = user
 item.save!
 
-item = Item.create! title: "[test] Blusa", description: "Blusa azul da GAP perdida na FEA", lost_date: Time.now.to_date.strftime("%d/%m/%Y"), lost: false
+item = Item.new title: "[test] Blusa", description: "Blusa azul da GAP perdida na FEA", lost_date: Time.now.to_date.strftime("%d/%m/%Y"), lost: false
 item.user = user2
 item.save!
 
-item = Item.create! title: "[test] Estojo", description: "Estojo azul com ziper preto", lost_date: Time.now.to_date.strftime("%d/%m/%Y"), lost: false
+item = Item.new title: "[test] Estojo", description: "Estojo azul com ziper preto", lost_date: Time.now.to_date.strftime("%d/%m/%Y"), lost: false
 item.user = user
 item.save!
 
-item = Item.create! title: "[test] Mochila", description: "Mochila amarela", lost_date: Time.now.to_date.strftime("%d/%m/%Y"), lost: false
+item = Item.new title: "[test] Mochila", description: "Mochila amarela", lost_date: Time.now.to_date.strftime("%d/%m/%Y"), lost: false
 item.user = user2
 item.save!
