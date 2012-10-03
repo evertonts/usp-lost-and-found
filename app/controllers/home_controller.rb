7a89aae5137bf24de22b6cdfdace5cffae5c4c70
Your Name <everton2x4@gmail.com>
2012-10-03 18:09:38 -0300

    Arrumada tagcloud

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,5 +4,12 @@ class HomeController < ApplicationController
     @item = Item.new
     @items_lost = Item.where(:lost => true).order("created_at desc").limit(10).reverse
     @items_found = Item.where(:lost => false).order("created_at desc").limit(10).reverse
+    @tags = Item.tag_counts_on(:tags)
+    
+    ActsAsTaggableOn::Tag.all.each do |tag|
+      unless @tags.include? tag
+        @tags << tag
+      end
+    end
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+# encoding: utf-8
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
@@ -5,6 +7,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.delete_all
 puts 'SETTING UP DEFAULT USER LOGIN'
 user = User.create! :name => 'First User', :email => 'user@example.com', :password => 'please', :password_confirmation => 'please'
 puts 'New user created: ' << user.name
@@ -14,6 +18,7 @@ user.add_role :admin
 user2.add_role :site_user
 
 
+Item.delete_all
 puts 'CREATING EXAMPLE ITEMS'
 item = Item.new title: "[test] Carteira", description: "Carteira preta com detalhes marrons", lost_date: Time.now.to_date.strftime("%d/%m/%Y"), lost: true
 item.user = user
@@ -54,3 +59,10 @@ item.save!
 item = Item.new title: "[test] Mochila", description: "Mochila amarela", lost_date: Time.now.to_date.strftime("%d/%m/%Y"), lost: false
 item.user = user2
 item.save!
+
+ActsAsTaggableOn::Tag.delete_all
+list = ['teste', 'informática', 'material escolar', 'roupa', 'documento']
+
+list.each do |tag|
+  ActsAsTaggableOn::Tag.find_or_create_by_name(:name => tag)
+end
