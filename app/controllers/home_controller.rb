a6d7bf246e8babe54535068608fdaa096f1c94e6
Your Name <everton2x4@gmail.com>
2012-10-14 20:26:54 -0300

    Ärrumado alguns detalhes na home e traduzida rota de contato

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,8 +2,8 @@ class HomeController < ApplicationController
   def index
     @users = User.all
     @item = Item.new
-    @items_lost = Item.where(:lost => true).order("created_at desc").limit(10).reverse
-    @items_found = Item.where(:lost => false).order("created_at desc").limit(10).reverse
+    @items_lost = Item.where(:lost => true, :returned => false).order("created_at desc").limit(10).reverse
+    @items_found = Item.where(:lost => false, :returned => false).order("created_at desc").limit(10).reverse
     @tags = Item.tag_counts_on(:tags) | ActsAsTaggableOn::Tag.all
 
   end
