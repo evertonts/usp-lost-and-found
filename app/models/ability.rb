8fa5a43abad0f3f4a1e82c8708e00c38a41f1ed1
Your Name <everton2x4@gmail.com>
2012-10-30 14:58:55 -0200

    Adicionando actions das tags para o can can

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -7,7 +7,7 @@ class Ability
       can :manage, :all
     else
       can :show, Item
-      can [:create, :new_lost, :new_found], Item
+      can [:create, :new_lost, :new_found, :autocomplete_tag_name, :tag], Item
       can [:search], Item
       
       can [:update, :destroy, :recover], Item do |item|
